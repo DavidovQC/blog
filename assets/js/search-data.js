89058209b434c97b1324144b951a82c9f730b036
DavidovQC <NathanDavidov9@gmail.com>
2025-08-29 13:58:02 +0000

    Deploying to gh-pages from @ DavidovQC/blog@375f1e255dee1abe68609ebafb74b22c7fda80b7 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -17,7 +17,7 @@ ninja.data = [{
             window.location.href = "/blog/posts/index.html";
           },
         },{id: "nav-resume",
-          title: "resume",
+          title: "Resume",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
